ddebef8ac6dc0536f4ae36bb01acda52b6792e36
BruninSouza <brun.souz4@gmail.com>
2025-04-09 15:14:25 -0300

    tranformando para react

--- assets/js/components/control-slider-item.js
+++
@@ -1,7 +0,0 @@
-const ControlSliderItem = (props) => {
-    return `
-        <button class="controls-slider_item" data-banner="btn-control"></button>
-    `
-}
-
-export default ControlSliderItem
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SearchMovie from './SearchMovie';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <SearchMovie />
+    </div>
+  );
+}
+
+export default App;
